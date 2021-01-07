707cd748d8a823615ffeeddd6dc37a147be392dd
AndresHMosqueda <andreshm94@gmail.com>
2021-01-06 23:13:01 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const myRef = useRef();
+  const [state, setState] = useState({
+    products: [
+      {
+        _id: "1",
+        title: "Nike Shoes",
+        src: [
+          "https://www.upsieutoc.com/images/2021/01/07/img2.png",
+          "https://www.upsieutoc.com/images/2021/01/07/img2.png",
+          "https://www.upsieutoc.com/images/2021/01/07/img3.png",
+          "https://www.upsieutoc.com/images/2021/01/07/img4.png",
+        ],
+        description: "UX desgining",
+        content: "Welcome to our channel...",
+        price: 23,
+        colors: ["red", "black", "crimson", "teal"],
+        count: 1,
+      },
+    ],
+    index: 0,
+  });
+
+  const { products, index } = state;
+
+  useEffect(() => {
+    const { index } = state;
+    myRef.current.children[index].className = "active";
+  }, []);
+
+  const handleTab = (index) => {
+    setState({ products, index: index });
+    const images = myRef.current.children;
+    console.log("imgs", images);
+    for (let i = 0; i < images.length; i++) {
+      images[i].className = images[i].className.replace("active", "");
+    }
+    images[index].className = "active";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.products.map((item) => (
+        <div className="details" key={item._id}>
+          <div className="big-img">
+            <img src={item.src[index]} alt="" />
+          </div>
+
+          <div className="box">
+            <div className="row">
+              <h2>{item.title}</h2>
+              <span>${item.price}</span>
+            </div>
+            <div className="colors">
+              {" "}
+              {item.colors.map((color, indx) => (
+                <button style={{ background: color }} key={indx}></button>
+              ))}{" "}
+            </div>
+            <p>{item.description}</p>
+            <p>{item.content}</p>
+            <div className="thumb" ref={myRef}>
+              {item.src.map((img, indx) => (
+                <img
+                  src={img}
+                  alt="img"
+                  key={indx}
+                  onClick={() => handleTab(indx)}
+                />
+              ))}
+            </div>
+            <button className="cart">Add to cart</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
